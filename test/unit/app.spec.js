d3c076321ababa511191794aa272412af82115e9
Patryk Hudy <Pato66>
2016-04-06 19:08:58 +0200

    add spy to isPalindrome

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -22,23 +22,83 @@ describe('app', function () {
     describe('isPalindrome', function () {
 
         describe('toHaveBeenCalled', function () {
-
+            beforeAll(function(){
+                spyOn(app, 'isPalindrome');
+                app.generateMessage('kajak');
+            });
+            it('should call isPalindrome function', function(){
+                expect(app.isPalindrome).toHaveBeenCalled();
+                expect(app.isPalindrome).toHaveBeenCalledWith('kajak');
+            });
         });
 
         describe('and.callThrough', function () {
-
+            beforeAll(function () {
+               spyOn(app, 'isPalindrome').and.callThrough();
+                app.generateMessage('wyraz');
+            });
+            it('should call isPalindrome function when generateMessage function is called',function(){
+                expect(app.isPalindrome).toHaveBeenCalled();
+                expect(app.isPalindrome).toHaveBeenCalledWith();
+            });
         });
 
         describe('and.returnValue', function () {
-
+            var czy_ok=true;
+            beforeAll(function(){
+               spyOn(app,'isPalindrome').and.returnValue(true);
+            });
+            it('should call isPalindrome and return true',function(){
+                 czy_ok = app.isPalindrome('zaraz');
+                expect(czy_ok).toBe(true);
+            });
+            it('should call generateMessage and isPalindrome should return true',function(){
+                czy_ok = app.generateMessage('zaraz');
+                expect(czy_ok).toEqual({vovel:2 ,palindrome:true})
+            });
         });
 
         describe('and.callFake', function () {
-
+            var czy_ok=true;
+            beforeAll(function(){
+                spyOn(app,'isPalindrome').and.callFake(function(str){
+                    var strTemp = 'kajak',  //str.toLowerCase(),
+                        strLength = strTemp.length;
+                    if (str === '') {
+                        return false;
+                    }
+                    var halfLength = (strLength % 2 === 0) ? (strLength / 2) : ((strLength - 1) / 2);
+                    for (var i = 0; i < halfLength; i++) {
+                        if (strTemp[i] !== strTemp.slice(-1 - i)[0]) {
+                            return false;
+                        }
+                    }
+                    return true;
+                });
+            });
+            it ('should call isPalindrome fake function',function(){
+                czy_ok= app.isPalindrome('nowy');
+                expect(czy_ok).toBe(true);
+            });
+            it('should call generateMessage and isPalindrome fake function',function(){
+                czy_ok = app.generateMessage('nowy');
+                expect(age).toEqual({vovel:1,palindrome:true})
+            });
         });
 
         describe('calls.count()', function () {
-
+            var czy_ok=true;
+            beforeAll(function(){
+                spyOn(app,'isPalindrome').and.callThrough();
+            });
+            it('should notice that isPalindrome function is call',function(){
+                czy_ok = app.isPalindrome('jeden');
+                expect(app.isPalindrome).calls.count().toBe(1);
+            });
+            it('should notice that isPalindrome is call when generateMesage is call',function(){
+                czy_ok=app.generateMessage('jeden');
+                expect(app.isPalindrome('jeden').calls.count()).toEqual(2);
+            });
         });
     });
 
